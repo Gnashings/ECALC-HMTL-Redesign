eaa48f4dec8aa551527562803fb3758e4260a737
Gnashings <Nashroy11@gmail.com>
2021-05-12 16:23:18 -0400

    Non-standard containers final

--- nonStandardContainer.js
+++ nonStandardContainer.js
@@ -3,22 +3,22 @@ var modal = document.getElementById("drink-modal");
 var modalimg = document.getElementById("drink-modal-img");
 var currentIDDrink;
 let drinks = [
-  ['Daiquiri', '1.5'],
-  ['Martini-Traditional', '1.2'],
-  ['Pina Colada', '2'],
-  ['Boot of Beer', '5-6'],
-  ['Cosmopolitan', '1.3'],
-  ['Margarita', '1.7'],
-  ['Gin & Tonic', '1.6'],
-  ['Manhattan', '2.5'],
-  ['Screwdriver', '1.3'],
-  ['Mojito', '1.3'],
-  ['Long Island', '1.5-4'],
-  ['Lemon Drop', '2.2'],
-  ['Pitcher of Beer', '4-5'],
-  ['Dry Martini', '1.4'],
-  ['Yard of beer', '5-6'],
-  ['Vodka Tonic', '1.3'],
+  ['Daiquiri','x 1.5'],
+  ['Martini Traditional', 'x 1.2'],
+  ['Pina Colada', 'x 2'],
+  ['Boot of Beer', 'x 5-6'],
+  ['Cosmopolitan', 'x 1.3'],
+  ['Margarita', 'x 1.7'],
+  ['Gin and Tonic', 'x 1.6'],
+  ['Manhattan', 'x 2.5'],
+  ['Screwdriver', 'x 1.3'],
+  ['Mojito', 'x 1.3'],
+  ['Long Island', 'x 1.5-4'],
+  ['Lemon Drop', 'x 2.2'],
+  ['Pitcher of Beer', 'x 4-5'],
+  ['Dry Martini', 'x 1.4'],
+  ['Yard of beer', 'x 5-6'],
+  ['Vodka Tonic', 'x 1.3'],
 ];
 // Get the button that opens the modal
 //var btn = document.getElementById("review");
@@ -32,11 +32,14 @@ function PopUpModal(clicked_id)
     var imageID=clicked_id;
 
     imageID = imageID.replace(/-/g, "")
+    var drinkDescID = clicked_id.toLowerCase() + "-description";
+    console.log(drinkDescID);
     currentIDDrink = removeHyphen.replace(/-/g, " ");
     modal.style.display = "block";
     document.getElementById("drink-modal-header").innerHTML = currentIDDrink;
-    document.getElementById("drink-modal-descriptor").innerHTML = "";
     modalimg.src = "images/container"+imageID+".png";
+    document.getElementById("drink-modal-descriptor").innerHTML = document.getElementById(drinkDescID).innerHTML;
+    document.getElementById("drink-modal-reveal").innerHTML = "x ?";
 }
 
 span.onclick = function() {
@@ -48,12 +51,13 @@ function Reveal() {
   for (i = 0; i < drinks.length; i++) {
     if(currentIDDrink == drinks[i][0])
     {
-      document.getElementById("drink-modal-descriptor").innerHTML = drinks[i][1];
+      document.getElementById("drink-modal-reveal").innerHTML = drinks[i][1];
       return;
     }
     else{
-      document.getElementById("drink-modal-descriptor").innerHTML = "NOT FOUND";
+      document.getElementById("drink-modal-reveal").innerHTML = "NOT FOUND";
       console.log(drinks[i][0]);
+      console.log(currentIDDrink);
     }
   }
 
